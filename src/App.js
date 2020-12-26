0253e96f3e4b6cbea82a283093fb5b1325de830f
JoeKabongo <jonathankabongo23@gmail.com>
2020-12-25 22:16:13 -0800

    setting up components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/navbar';
+import About from './components/about';
+import Article from './components/article';
+import Blog from './components/blog';
+import Home from './components/home';
+import Projects from './components/projects';
+import SingleProject from './components/single-project';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/projects/:id">
+          <SingleProject />
+        </Route>
+        <Route path="/blog">
+          <Blog />
+        </Route>
+        <Route path="/blog/:id">
+          <Article />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/single-project.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SingleProject() {
+  return <h1> Single Project</h1>;
+}
+
+export default SingleProject;
